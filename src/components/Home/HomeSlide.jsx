5eb64c0baf0fd3dd5362fc663a27212b12f8933e
ThanhNhut <nhut456822@donga.edu.vn>
2022-05-17 09:50:42 +0700

    update Image

--- src/components/Home/HomeSlide.jsx
+++ src/components/Home/HomeSlide.jsx
@@ -1,6 +1,8 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Button from "../Button/Button";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '../Button/Button';
+
+import bannerImage from '../../assets/images/banner-dog-cat.jpg';
 
 const HomeSlide = (props) => {
 	const { activeDot, handleActiveDot } = props;
@@ -10,10 +12,7 @@ const HomeSlide = (props) => {
 			<div className="home-slide__image">
 				<div className="home-slide__image__item">
 					{activeDot ? (
-						<img
-							src="https://www.loveyourdog.com/wp-content/uploads/2020/04/Dog-Eating-Cat-Food.jpg"
-							alt=""
-						/>
+						<img src={bannerImage} alt="" />
 					) : (
 						<img
 							src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F24%2F2021%2F02%2F05%2FGettyImages-80285230-2000.jpg"
@@ -25,7 +24,7 @@ const HomeSlide = (props) => {
 			<div className="home-slide__dots">
 				<p
 					className={`home-slide__dots__item ${
-						activeDot ? "active" : ""
+						activeDot ? 'active' : ''
 					}`}
 					onClick={() => handleActiveDot(1)}
 				>
@@ -33,7 +32,7 @@ const HomeSlide = (props) => {
 				</p>
 				<p
 					className={`home-slide__dots__item ${
-						!activeDot ? "active" : ""
+						!activeDot ? 'active' : ''
 					}`}
 					onClick={() => handleActiveDot(0)}
 				>
